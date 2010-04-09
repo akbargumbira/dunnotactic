ac4328b89d57535be5d0053d5207f995eda5faa2
gumbira.mymind <gumbira.mymind@683c1cbb-0ac3-0733-434b-7841ce19f6b5>
2010-04-09 07:03:44 +0000

    Ada komennya

--- trunk/Tugas Besar/Map/Map.h
+++ trunk/Tugas Besar/Map/Map.h
@@ -14,25 +14,25 @@ using namespace std;
 class Map
 {
 public :
-    static const int default_size = 30;
-    Map(); //constructor default
-    Map (const Map&); //Copy Constructor
-    virtual ~Map();//Destructor
+    static const int default_size = 30; //Ukuran dari map dibuat default yaitu 30x30
+    Map(); //constructor default Map
+    Map (const Map&); //Copy Constructor Map
+    virtual ~Map();//Destructor Map
 
-    void CreateMap(const int& X, const int& Y); //udah pake throw
-    void Load(string sin); //udah pake throw
-    void Save(string s);
-    void SetTerrainXY(int X,int Y, string GridContent); //udah pake throw
-    void SetTerrainArea(int X1,int Y1,int X2,int Y2, string GridContent); //udah pake throw
-    void SetMapRandom();
-    int GetTerrain(int X, int Y);
-    int GetSizeX();
-    int GetSizeY();
+    void CreateMap(const int& X, const int& Y); // Method yang digunakan untuk merekonstruksi ulang Map
+    void Load(string sin); // Method yang digunakan untuk meload file .map
+    void Save(string s); // Method yang digunakan untuk menyimpan map
+    void SetTerrainXY(int X,int Y, string GridContent); // Method yang digunakan untuk mengeset isi terrain
+    void SetTerrainArea(int X1,int Y1,int X2,int Y2, string GridContent); // Method yang digunakan untuk mengeset isi terrain dari suatu area
+    void SetMapRandom(); // Method yang digunakan untuk mengeset terrain dari map secara random
+    int GetTerrain(int X, int Y); // Method yang digunakan untuk mendapatkan isi suatu terrain dari suatu koordinat map
+    int GetSizeX(); // Method yang digunakan untuk mendapatkan SizeX dari Map
+    int GetSizeY(); // Method yang digunakan untuk mendapatkan SizeY dari Map
 
 private :
-    int SizeX;
-    int SizeY;
-    int Terrain[default_size][default_size];
+    int SizeX; // Ukuran X dari Map yang efektif terpakai
+    int SizeY; // Ukuran Y dari Map yang efektif terpakai
+    int Terrain[default_size][default_size]; // Matriks dari terrain yang digunakan untuk menyimpan isi dari terrain
 };
 
 #endif	/* _MAP_H */
