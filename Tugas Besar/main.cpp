3714734a5ec2bb6593c3e30f9cf3a2ad88f5162d
rezanachmad <rezanachmad@683c1cbb-0ac3-0733-434b-7841ce19f6b5>
2010-04-13 18:17:49 +0000

    final

--- Tugas Besar/main.cpp
+++ Tugas Besar/main.cpp
@@ -16,63 +16,7 @@
 int main(int argc, char** argv) {
 
     DunnoTactic::Main();
-    /*
-    vector<int> available;
-
-    available.push_back(1);
-    available.push_back(5);
-    available.push_back(7);
-    available.push_back(9);
-
-    for (int i=0;i<available.size();++i)
-    {
-        cout << i << " " << available[i] << endl;
-    }
-
-    vector<int>::iterator begin = available.begin();
-    begin = begin + 2;
-
-    available.erase(begin);
-
-    for (int i=0;i<available.size();++i)
-    {
-        cout << i << " " << available[i] << endl;
-    }*/
-
-/*
-    DunnoTactic::P1.push_back(new Knight("human"));
-    DunnoTactic::P2.push_back(new Assassin("elf"));
-    DunnoTactic::P1.push_back(new Assassin("human"));
-
-    vector<Job*>::iterator R;
-    vector<Job*>::iterator S;
-
-    R = DunnoTactic::P1.begin()+1;
-    S = DunnoTactic::P2.begin();
-
-    cout << (*R)->GetJobName() << endl;
-    cout << (*R)->GetAttackPoint() << endl;
-    cout << (*S)->GetHP() << endl;
-    cout << (*S)->GetSP() << endl;
-    cout << (*S)->GetDefensePoint() << endl;
-    (*R)->Attack(*(*S));
-    cout << (*S)->GetHP() << endl;
-   */
-/*
-    Map M;
-    try
-    {
-        M.SetMapRandom();
-        //M.Save("Cob");
-        M.Load("Coba");
-        M.SetTerrainXY(31,31,3);
-        M.Save("Voba2");
-    }
-    catch (const char* e)
-    {
-        cout << e << endl;
-
-    }*/
+    
     return (EXIT_SUCCESS);
 }
 
